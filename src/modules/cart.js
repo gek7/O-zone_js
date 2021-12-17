2a3675066c8234dfeae4f0756213120604a96d2f
gek7 <evgenuy2000a@mail.ru>
2021-12-17 23:48:50 +0300

    вопрос

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -4,7 +4,7 @@ const cart = () => {
     const cartCloseBtn = document.querySelector(".cart-close");
 
     const openCart = () => {
-        cartModal.style.display = 'flex';
+        cartModal.style.display = 'flex'; //Что будет если тут будет группа селекторов?
     };
 
     const closeCart = () => {
